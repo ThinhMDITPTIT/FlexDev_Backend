2ed779a9503e4069025ddc29c4bafbabad7c9dc1
ThinhMD <ThinhMDITPTIT@gmail.com>
2021-11-11 21:29:35 +0700

    Try some config - ThinhMD

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
-var jwt = require('express-jwt');
 var secret = require('../config').secret;
+var jwt = require('express-jwt');
 
 function getTokenFromHeader(req){
   if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Token' ||
--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 var router = require('express').Router();
 
 router.use('/api', require('./api'));
