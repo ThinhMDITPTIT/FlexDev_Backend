93a26b953ea9a5701ff980040af9586444778e1e
ThinhMD <ThinhMDITPTIT@gmail.com>
2021-11-11 21:11:08 +0700

    Change port - ThinhMD

--- app.js
+++ app.js
@@ -80,5 +80,5 @@ app.use(function(err, req, res, next) {
 
 // finally, let's start our server...
 var server = app.listen( process.env.PORT || 3000, function(){
-  console.log('Listening on port ' + server.address().port);
+  console.log('Listening on port ' + server.address().port, app.settings.env);
 });
