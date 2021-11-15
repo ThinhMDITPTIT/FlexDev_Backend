0472b10dca823c6de1859174409abed3884a85a4
ThinhMD <ThinhMDITPTIT@gmail.com>
2021-11-15 18:28:29 +0700

    Change folder code - ThinhMD

--- models/Comment.js
+++ models/Comment.js
@@ -1,21 +1,19 @@
 var mongoose = require('mongoose');
 
-var CommentSchema = new mongoose.Schema(
-  {
-    body: String,
-    author: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-    article: { type: mongoose.Schema.Types.ObjectId, ref: 'Article' },
-  },
-  { timestamps: true }
-);
+var CommentSchema = new mongoose.Schema({
+  body: String,
+  author: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  article: { type: mongoose.Schema.Types.ObjectId, ref: 'Article' }
+}, {timestamps: true});
 
 // Requires population of author
-CommentSchema.methods.toJSONFor = function (user) {
+CommentSchema.methods.toJSONFor = function(user){
   return {
-    id: this._id,
+    _id: this._id,
     body: this.body,
     createdAt: this.createdAt,
-    author: this.author.toProfileJSONFor(user),
+    author: this.author,
+    article: this.article,
   };
 };
 
